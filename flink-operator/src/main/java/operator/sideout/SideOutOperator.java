c9c240e6b05b45cba603edd5298a56fe28c084f1
jaydon <245831311@qq.com>
2021-11-26 18:24:29 +0800

    提交sideout demo

---
+++ flink-operator/src/main/java/operator/sideout/SideOutOperator.java
@@ -0,0 +1,37 @@
+package operator.sideout;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+public class SideOutOperator {
+	
+	final static OutputTag<String> sideOutputTag = new OutputTag<String>("test");
+	
+	public static void main(String[] args) {
+		
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		
+		DataStream<String> socketDs = env.socketTextStream("localhost", 9999);
+		
+		DataStream<String> socketDs.flatMap(new FlatMapFunction<String, String>() {
+			@Override
+			public void flatMap(String value, Collector<String> out) throws Exception {
+				String[] str = value.split(",");
+				for(String word : str){
+					out.collect(word);
+				}
+			}
+		}).process(new ProcessFunction<String, String>() {
+			@Override
+			public void processElement(String value, Context context,
+					Collector<String> out) throws Exception {
+				context.output(sideOutputTag, value);
+			}
+		});
+	}
+
+}
