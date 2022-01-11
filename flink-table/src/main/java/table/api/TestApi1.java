1e4ac45a1549fe5b3e41a3d339d5611549937ef0
jaydon <245831311@qq.com>
2022-01-11 19:37:47 +0800

    增加table包

---
+++ flink-table/src/main/java/table/api/TestApi1.java
@@ -0,0 +1,10 @@
+package table.api;
+
+import org.apache.flink.table.api.TableEnvironment;
+
+public class TestApi1 {
+
+	public static void main(String[] args) {
+		//TableEnvironment tableEnv = TableEnvironment.create(null);
+	}
+}
---
+++ flink-table/src/main/java/table/window/TumbleWindow.java
@@ -0,0 +1,8 @@
+package table.window;
+
+public class TumbleWindow {
+
+	public static void main(String[] args) {
+		
+	}
+}
