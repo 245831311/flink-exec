592576c99eb42a5c3ae42833150404599faa0179
jaydon <245831311@qq.com>
2022-01-15 20:15:02 +0800

    提交table test

--- flink-table/src/main/java/table/api/TestApi1.java
+++ flink-table/src/main/java/table/api/TestApi1.java
@@ -1,10 +1,41 @@
 package table.api;
 
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.types.Row;
 
+/**
+ * 使用简单用法
+ * @author Lenovo
+ *
+ */
 public class TestApi1 {
 
 	public static void main(String[] args) {
-		//TableEnvironment tableEnv = TableEnvironment.create(null);
+		EnvironmentSettings settings = EnvironmentSettings.newInstance().inBatchMode().build();
+		TableEnvironment tableEnv = TableEnvironment.create(settings);
+		
+		Table table =  tableEnv.fromValues(
+				DataTypes.ROW(
+				        DataTypes.FIELD("id", DataTypes.DECIMAL(10, 2)),
+				        DataTypes.FIELD("name", DataTypes.STRING())
+				    ),
+				Row.of(1L,"ABC"),
+				Row.of(2L,"ABCD"),
+				Row.of(3L,"ABC")
+		);
+		//tableEnv.registerTable("WordCount",table);
+		tableEnv.createTemporaryView("wc",table);
+		Table count = tableEnv.from("wc")
+				.groupBy("name").select("name,id.sum as count");
+		
+		Table sqlCount = tableEnv.sqlQuery("select sum(id) as c from wc group by(name) ");
+		
+		
+		sqlCount.execute().print();
 	}
+	
+	
 }
---
+++ flink-table/src/main/java/table/sources/fileSources.java
@@ -0,0 +1,27 @@
+package table.sources;
+
+import org.apache.flink.connector.datagen.table.DataGenConnectorOptions;
+import org.apache.flink.streaming.api.functions.source.datagen.DataGenerator;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.TableDescriptor;
+import org.apache.flink.table.api.TableEnvironment;
+
+public class fileSources {
+
+	public static void main(String[] args) {
+		
+		EnvironmentSettings settings = EnvironmentSettings.newInstance().inBatchMode().build();
+		TableEnvironment tableEnv = TableEnvironment.create(settings);
+		
+		tableEnv.createTemporaryTable("csvSourceTable", TableDescriptor
+				.forConnector("datagen").schema(Schema.newBuilder()
+		        .column("f0", DataTypes.STRING())
+		        .column("f1", DataTypes.STRING())
+		        .build())
+				.option(DataGenConnectorOptions.ROWS_PER_SECOND, 100L)
+			    .build());
+				
+	}
+}
